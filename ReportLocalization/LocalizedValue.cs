11039c419086402d414136343237b9643d9e5a2e
UmaMaheshK <uma.kondapally@gmail.com>
2017-06-06 17:46:31 +0530

    Add project files.

---
+++ ReportLocalization/LocalizedValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ReportLocalization
+{
+    public static class LocalizedValue
+    {
+        public static string GetString(string name, string cultureInfoName)
+        {
+            string returnString = string.Empty;
+            try
+            {
+                // Culture formation  
+                CultureInfo ci = new CultureInfo(cultureInfoName);
+                // Value retrieved from the resource file  
+                returnString = (cultureInfoName == "hi-IN") ? Hindi.ResourceManager.GetString(name, ci) : English.ResourceManager.GetString(name, ci);
+            }
+            catch (Exception ex) { }
+            if (string.IsNullOrEmpty(returnString))
+            {
+                returnString = name;
+            }
+            return returnString;
+        }
+    }
+}
